29a2758de09edc8711bc11be892446d50ed50fbc
liangzhihao <zhihao.l@aliyun.com>
2020-02-24 18:16:02 +0800

    0224

---
+++ design/homework-administration-back/src/main/java/io/lzh/homeworkadministrationback/HomeworkAdministrationBackApplication.java
@@ -0,0 +1,13 @@
+package io.lzh.homeworkadministrationback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkAdministrationBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkAdministrationBackApplication.class, args);
+    }
+
+}
---
+++ design/homework-administration-back/src/test/java/io/lzh/homeworkadministrationback/HomeworkAdministrationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lzh.homeworkadministrationback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkAdministrationBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
