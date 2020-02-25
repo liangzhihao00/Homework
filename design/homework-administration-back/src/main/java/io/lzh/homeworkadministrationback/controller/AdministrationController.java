1e3ff43bc4f9c0f8878944b2dc7291f926e1a01e
liangzhihao <zhihao.l@aliyun.com>
2020-02-25 16:47:05 +0800

    0225

--- design/homework-administration-back/src/main/java/io/lzh/homeworkadministrationback/controller/AdministrationController.java
+++ design/homework-administration-back/src/main/java/io/lzh/homeworkadministrationback/controller/AdministrationController.java
@@ -1,11 +1,13 @@
 package io.lzh.homeworkadministrationback.controller;
 
-import io.lzh.homeworkadministrationback.dto.in.AdministratorLoginInDTO;
-import io.lzh.homeworkadministrationback.dto.in.AdministratorResetPwdEmailInDTO;
-import io.lzh.homeworkadministrationback.dto.in.AdministratorResetPwdMobileInDTO;
-import io.lzh.homeworkadministrationback.dto.in.AdministratorUpdateProfileInDTO;
+import io.lzh.homeworkadministrationback.dto.in.*;
 import io.lzh.homeworkadministrationback.dto.out.AdministratorGetProfileOutDTO;
+import io.lzh.homeworkadministrationback.dto.out.AdministratorListOutDTO;
+import io.lzh.homeworkadministrationback.dto.out.AdministratorShowOutDTO;
+import io.lzh.homeworkadministrationback.dto.out.PageOutDTO;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/administration")
@@ -17,31 +19,52 @@ public class AdministrationController {
     }
 
     @GetMapping("/getProfile")
-    public AdministratorGetProfileOutDTO getProfile(Integer adminstratorId){
+    public AdministratorGetProfileOutDTO getProfile(@RequestParam(required = false) Integer adminstratorId){
         return null;
     }
 
-    @PostMapping("/updateProdfile")
-    public void updateProdfile(@RequestBody AdministratorUpdateProfileInDTO administratorUpdateProfileInDTO){
-
-    }
-    @GetMapping("/sendPasswordResetCodeToEmail")
-    public void sendPasswordResetCodeToEmail(@RequestParam String email){
+    @PostMapping("/updateProfile")
+    public void updateProfile(@RequestBody AdministratorUpdateProfileInDTO administratorUpdateProfileInDTO){
 
     }
 
-    @PostMapping("/resetPassword")
-    public void resetPasswordByEmail(@RequestBody AdministratorResetPwdEmailInDTO administratorResetPwdEmailInDTO){
+    @GetMapping("/getPwdResetCode")
+    public String getPwdResetCode(@RequestParam String email){
+        return null;
+    }
+
+    @PostMapping("/resetPwd")
+    public void resetPwd(@RequestBody AdministratorResetPwdInDTO administratorResetPwdInDTO){
 
     }
 
-    @GetMapping("/sendPasswordResetCodeToMobile")
-    public void sendPasswordResetCodeToMobile(@RequestParam String mobile){
+    @GetMapping("/getList")
+    public PageOutDTO<AdministratorListOutDTO> getList(@RequestParam Integer pageNum){
+        return null;
+    }
+
+    @GetMapping("/getById")
+    public AdministratorShowOutDTO getById(@RequestParam Integer administratorId){
+        return null;
+    }
+
+    @PostMapping("/create")
+    public Integer create(@RequestBody AdministratorCreateInDTO administratorCreateInDTO){
+        return null;
+    }
+
+    @PostMapping("/update")
+    public void update(@RequestBody AdministratorUpdateInDTO administratorUpdateInDTO){
 
     }
 
-    @PostMapping("/resetPassword")
-    public void resetPasswordByMobile(@RequestBody AdministratorResetPwdMobileInDTO administratorResetPwdMobileInDTO){
+    @PostMapping("/delete")
+    public void delete(@RequestBody Integer adminstratorId){
+
+    }
+
+    @PostMapping("/batchDelete")
+    public void batchDelete(@RequestBody List<Integer> administratorIds){
 
     }
 }
