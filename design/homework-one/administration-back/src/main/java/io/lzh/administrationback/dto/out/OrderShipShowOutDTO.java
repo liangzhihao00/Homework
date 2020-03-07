6c76a1c86d414bb1de9088dea91f901d20956049
LZH1204 <1377381107@qq.com>
2020-03-07 08:51:54 +0800

    design+db.sql

--- design/homework-one/administration-back/src/main/java/io/lzh/administrationback/dto/out/OrderShipShowOutDTO.java
+++
@@ -1,4 +0,0 @@
-package io.lzh.administrationback.dto.out;
-
-public class OrderShipShowOutDTO {
-}
--- design/jcart-administration-front/assets/js/common.js
+++
@@ -1,2 +0,0 @@
-axios.defaults.baseURL = 'http://localhost:8080';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
