f674baadd3182337376b19bed0fe536294360460
LZH1204 <1377381107@qq.com>
2020-03-09 09:02:47 +0800

    administration-dto

---
+++ administration-back/src/main/java/io/lzh/administrationback/dto/in/AdministratorUpdateInDTO.java
@@ -0,0 +1,58 @@
+package io.lzh.administrationback.dto.in;
+
+public class AdministratorUpdateInDTO {
+    private Integer administratorId;
+    private String realName;
+    private String password;
+    private String email;
+    private String avatarUrl;
+    private Byte status;
+
+    public Integer getAdministratorId() {
+        return administratorId;
+    }
+
+    public void setAdministratorId(Integer administratorId) {
+        this.administratorId = administratorId;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+}
