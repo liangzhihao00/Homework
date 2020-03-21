0cfe58e671402a369a22c10937789c57b8bb09ac
LZH1204 <1377381107@qq.com>
2020-03-21 17:55:52 +0800

    添加jwt工具中的包和创建customerLoginVo

--- store-back/src/main/java/io/lzh/storeback/util/JWTUtil.java
+++ store-back/src/main/java/io/lzh/storeback/util/JWTUtil.java
@@ -1,5 +1,21 @@
 package io.lzh.storeback.util;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import io.lzh.storeback.dto.out.CustomerLoginOutDTO;
+import io.lzh.storeback.po.Customer;
+import io.lzh.storeback.vo.CustomerLoginVO;
+import io.lzh.storeback.dto.out.CustomerLoginOutDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+
 @Component
 public class JWTUtil {
 
